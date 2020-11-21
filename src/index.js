9e3a363d58ad28d76e275938be0a05c0e6912db1
Mikael Josias <mikaeljosias2002@gmail.com>
2020-11-21 10:48:53 -0300

    refactor(index): update routes

--- src/index.js
+++ src/index.js
@@ -6,4 +6,6 @@ app.get('/', (require, response) => {
     return response.json({message: 'Hello World'});
 });
 
-app.listen(3333);
+app.listen(3333, () => {
+    console.log('🚀 Back-end started✔✔✔');
+});
